8fb46e29a8602652a3d247334eee32cf184e4db3
Paul Leonard <paul_leonard88@hotmail.com>
2025-10-06 19:19:04 +0200

    update

--- greeting.py
+++ greeting.py
@@ -2,9 +2,8 @@ name = "Sarah"
 print("Hello, " + name + "!")
 
 hello = "Hello"
-name = "Sarah"
 greeting = hello + ", " + name + "!"
 print(greeting)
 
-myname = input("What is your name? ")
-print("Hello, " + myname + "!")
+myname = input("What is your name?\n ")
+myname = input("What is your name?\n")
